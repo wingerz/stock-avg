3f2ce9200979be43e3cf6d1c0ec71915ea467e92
Wing Yung <wing@yelp.com>
2013-11-09 15:24:23 -0800

    calculate 10-month avg

--- src/stock_avg/core.clj
+++ src/stock_avg/core.clj
@@ -44,13 +44,23 @@
            data-in-range))
      (count data-in-range))))
 
-(defn percent-diff [current comparison]
-  (*
-   (/
-    (- current comparison)
-    comparison
-    )
-   100))
+(defn year-and-month [date]
+  (let [date-ymd (map (fn [x] (. Integer parseInt x)) (clojure.string/split date #"-"))]
+    (vector (first date-ymd) (second date-ymd))))
+
+(defn month-ends [data]
+  (let [group-data (group-by 
+                    (fn [data-for-day]
+                      (year-and-month (:date data-for-day)))
+                    data)
+        months (reverse (sort (keys group-data)))
+        ]
+    (rest ; don't include the current month
+     (map
+      (fn [x]
+        (first (group-data x)))
+      months))
+    ))
 
 (defn calculate-things [data]
   (let
@@ -58,14 +68,23 @@
        previous (-> data second :close read-string)
        recent-change (- recent previous)
        avg-200 (moving-avg data 200)
-       avg-300 (moving-avg data 300)]
+       month-ends (month-ends data)
+       recent-month-end (-> month-ends first :close read-string)
+       month-ends-10 (take 10 month-ends)
+       month-ends-avg-10 (/ (reduce +
+                                       (map
+                                        (fn [x]
+                                          (read-string (:close x)))
+                                        month-ends-10))
+                               (count month-ends-10))
+       ]
     {
      :recent recent
      :recent-change recent-change
      :moving-avg-200 avg-200
      :diff-200 (/ recent-change avg-200)
-     :moving-avg-300 avg-300
-     :diff-300 (/ recent-change avg-300)
+     :month-end recent-month-end
+     :month-ends-avg-10 month-ends-avg-10
      :count (count data)
      }))
 
@@ -78,18 +97,18 @@
 (defn is-special-condition [calculated-data]
   (or
    (> (math/abs (:diff-200 calculated-data)) 0.01)
-   (> (math/abs (:diff-300 calculated-data)) 0.01)))
+   (< (:month-end calculated-data) (:month-ends-avg-10 calculated-data))))
 
 (defn output-header []
   (clojure.string/join
    "\t"
    [
     "sym"
+    "month"
+    "10month"
     "recent"
     "change"
     "200d"
-    "% vs"
-    "300d"
     "% vs"
     "of note"
     "days"
@@ -101,18 +120,17 @@
    "\t"
    [
     ticker
+    (format-price (:month-end calculated-data))
+    (format-price (:month-ends-avg-10 calculated-data))
     (format-price (:recent calculated-data))
     (format-price (:recent-change calculated-data))
     (format-price (:moving-avg-200 calculated-data))
     (format-percent (:diff-200 calculated-data))
-    (format-price (:moving-avg-300 calculated-data))
-    (format-percent (:diff-300 calculated-data))
     (cond
      (is-special-condition calculated-data) "*"
      :else "")
     (cond
      (< (:count calculated-data) 200) "lt200"
-     (< (:count calculated-data) 300) "lt300"
      :else "")
     ]))
 
@@ -146,4 +164,4 @@
        (map (fn [ticker]
              (output-ticker ticker ((symbol ticker) ticker-data))
              ) 
-           tickers))))))
+            tickers))))))
